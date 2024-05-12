0234d8a41130341d07a3f0902bd5f513fcf1a5e0
Maria Ingold <maria@mireality.co.uk>
2024-05-12 05:10:56 +0100

    Descriptive and Inferential a

--- 4_NA_StatisticalAnalysis.r
+++ 4_NA_StatisticalAnalysis.r
@@ -52,7 +52,7 @@ View(hse_data)       # View dataset
 glimpse(hse_data)    # Structure of dataset
 summary(hse_data)    # Summary of dataset
 
-# Create list of names with thier labels for reference
+# Create list of names with their label for reference
 hse_names <- names(hse_data)
 hse_labels <- sapply(hse_data,
                  function(x) {
@@ -105,16 +105,19 @@ mode_crv <- function(x) {
 # Age: Age at last birthday (some ages equal 0, due to babies under 12 months)
 ##############################################################################
 
+##############################################################################
 # a: How many people are included in the sample?
 # ==> Outcome: 10,617 people are included in the sample.
 # pserial is the "Serial number of Individual"
 # There are no empty rows (NA)
 # There are no duplicates (TRUE means no duplicates)
 # This matches the number of rows in the dataset.
+##############################################################################
 any(is.na(hse_data$pserial))       # Check for empty rows (NA)
 all(!duplicated(hse_data$pserial)) # Check for duplicates
 sum(!is.na(hse_data$pserial))      # Number of not empty rows
 
+##############################################################################
 # b: What is the percentage of people who drink alcohol? (dnnow)
 # ==> Outcome: 78.65% of people who responded yes or no drink alcohol.
 # dnnow is "Whether drink nowadays"
@@ -123,6 +126,7 @@ sum(!is.na(hse_data$pserial))      # Number of not empty rows
 # Frequency table ==> Yes = 6712, No = 1822
 # Percentage ==> Yes = 78.65%, No = 21.35%
 # Percentage Yes = (6712 / (6712 + 1822)) * 100
+##############################################################################
 sum(is.na(hse_data$dnnow))                              # Number empty rows (NA)
 table(as_factor(hse_data$dnnow))                        # Frequency table
 dnnow_pct_tab <- table(as_factor(hse_data$dnnow)) %>%
@@ -158,6 +162,7 @@ dnnow_pct_df %>%
        y = dnnow_y_label,
        fill = "Drink nowadays")
 
+##############################################################################
 # c: What is the percentage of women in the sample? (Sex)
 # ==> Outcome: 54.30% of the sample are women.
 # Male (1), Female (2)
@@ -165,10 +170,12 @@ dnnow_pct_df %>%
 # Frequency table ==> Male = 4852, Female = 5765
 # Percentage Female = 54.29971
 # Percentage Female = (5765 / (4852 + 5765)) * 100
+##############################################################################
 sum(is.na(hse_data$Sex))                                # Number empty rows (NA)
 table(as_factor(hse_data$Sex))                          # Frequency table
 table(as_factor(hse_data$Sex)) %>% prop.table() * 100   # Percentage
 
+##############################################################################
 # d: What is the highest educational level? (topqual3)
 # ==> Outcome: The highest educational level is NVQ4/NVQ5/Degree or equiv (1)
 # topqual3 is "Highest educational level"
@@ -180,6 +187,7 @@ table(as_factor(hse_data$Sex)) %>% prop.table() * 100   # Percentage
 # Foreign/other (6) = 127 people
 # No qualification (7) = 2037 people
 # Number of empty rows (NA) ==> 2051
+##############################################################################
 attr(hse_data$topqual3, "labels")   # Labels
 sum(is.na(hse_data$topqual3))       # Number empty rows (NA)
 table(as_factor(hse_data$topqual3)) # Frequency table
@@ -216,6 +224,7 @@ topqual_pct_df %>%
        y = topqual_y_label,
        fill = "Educational level")
 
+##############################################################################
 # e: What is percentage of divorced and separated people? (marstatc)
 # ==> Outcome: 9.50 % of the sample are divorced and separated.
 # marstatc is "Marital status including cohabitees"
@@ -227,6 +236,7 @@ topqual_pct_df %>%
 # Widowed (6)
 # Cohabitees (7)
 # Number of empty rows (NA) ==> 2009
+##############################################################################
 attr(hse_data$marstatc, "labels") # Labels
 sum(is.na(hse_data$marstatc))                              # NA
 table(as_factor(hse_data$marstatc))                        # Frequency table
@@ -265,10 +275,12 @@ mar_pct_df %>%
        y = mar_y_label,
        fill = "Marital status")
 
+##############################################################################
 # Try a Venn Diagram of dnnow = Yes (1), # topqual3 = NVQ4/NVQ5/Degree or equiv (1)
 # and marstatc = Divirced (5) or Separated (4)
 # Do one for Sex = Female (2)
 # Do one for Sex = Male (1)
+##############################################################################
 
 # Create vectors for Venn Diagram
 dnnow_yes_serials <- hse_data$pserial[hse_data$dnnow == 1]
@@ -281,12 +293,12 @@ serials_drink_female <- list("Drink" = dnnow_yes_serials,
                              "Female" = female_serials)
 serials_drink_male <- list("Drink" = dnnow_yes_serials,
                             "Male" = male_serials)
-serials_drink_mf <- list("Drink" = dnnow_yes_serials, 
+serials_drink_mf <- list("Drink" = dnnow_yes_serials,
                          "Female" = female_serials,
                          "Male" = male_serials)
-serials_nodrink_mf <- list ("No Drink" = dnnow_no_serials, 
+serials_nodrink_mf <- list ("No Drink" = dnnow_no_serials,
                             "Female" = female_serials,
-                            "Male" = male_serials)                           
+                            "Male" = male_serials)
 serials_list_female <- list("Drink" = dnnow_yes_serials,
                             "Female" = female_serials,
                             "Top Qualification" = topqual_serials,
@@ -334,9 +346,12 @@ ggVennDiagram(serials_list_male,
   ggplot2::scale_fill_gradient(low = "#EFF3FF", high = "#4292c6") +
   ggtitle("Venn Diagram for Males")
 
+##############################################################################
 # f: Find the mean, median, mode, minimum, maximum, range and standard deviation
 #    of household size, BMI and age at last birthday (HHSize, bmival, Age)
+##############################################################################
 
+##############################################################################
 # HHSize: Household size
 # Number empty rows (NA) = 0
 # ==> Outcome:
@@ -347,11 +362,33 @@ ggVennDiagram(serials_list_male,
 # Max = 10
 # Range = 9
 # Standard Deviation = 1.37
+##############################################################################
 sum(is.na(hse_data$HHSize)) # Number empty rows (NA)
 summary(hse_data$HHSize)    # Summary statistics
 describe(hse_data$HHSize)   # Descriptive statistics
 mode(hse_data$HHSize)       # Mode
+table(hse_data$HHSize)      # Frequency table
 
+# Plot box plot (check for outliers)
+hse_data %>% 
+  ggplot(aes(y = HHSize)) +
+  geom_boxplot(fill = "lightblue", outlier.color = "red", na.rm = TRUE) +
+  theme_bw() +
+  labs(title = "Household Size Box Plot",
+       y = "Size") +
+  scale_x_continuous(breaks = NULL) # Remove x-axis labels  
+
+# Plot frequency table
+hse_data %>% 
+  mutate(HHSize = as.factor(HHSize)) %>% # Convert to factor for discrete
+  ggplot(aes(x = HHSize)) +
+  geom_bar(fill = "lightblue") +
+  theme_bw() +
+  labs(title = "Household Size Frequency Table",
+       x = "Size",
+       y = "Frequency")
+
+##############################################################################
 # bmival: Body Mass Index
 # Number empty rows (NA) = 2241
 # ==> Outcome:
@@ -362,15 +399,26 @@ mode(hse_data$HHSize)       # Mode
 # Max = 65.28
 # Range = 56.94
 # Standard Deviation = 6.14
+##############################################################################
 sum(is.na(hse_data$bmival)) # Number empty rows (NA)
 summary(hse_data$bmival)    # Summary statistics
 describe(hse_data$bmival)   # Descriptive statistics
+
+# Plot box plot (check for outliers)
+hse_data %>%
+  ggplot(aes(y = bmival)) +
+  geom_boxplot(fill = "lightblue", outlier.color = "red", na.rm = TRUE) +
+  theme_bw() +
+  labs(title = "BMI Box Plot",
+       y = "Size") +
+  scale_x_continuous(breaks = NULL) # Remove x-axis labels
+
 # mode() function does not work for continuous random variables, need PDF plot
 # Plot density
 # Creates a right-skewed, single mode plot
 hse_data %>%
   ggplot(aes(x = bmival)) +
-  geom_density(fill = "gray", alpha=0.5) +
+  geom_density(fill = "lightblue", alpha = 0.5) +
   theme_bw() +
   labs(title = "BMI Density plot",
        x = "BMI",
@@ -378,6 +426,7 @@ hse_data %>%
 # There is only one mode, so use the density plot to estimate it
 mode_crv(hse_data$bmival)   # Estimated mode
 
+##############################################################################
 # Age: Age at last birthday (some ages equal 0, due to babies under 12 months)
 # Number empty rows (NA) = 0
 # ==> Outcome:
@@ -388,6 +437,7 @@ mode_crv(hse_data$bmival)   # Estimated mode
 # Max = 100
 # Range = 100
 # Standard Deviation = 23.83
+##############################################################################
 sum(is.na(hse_data$Age)) # Number empty rows (NA)
 summary(hse_data$Age)    # Summary statistics
 describe(hse_data$Age)   # Descriptive statistics
@@ -396,11 +446,96 @@ mode(hse_data$Age)       # Mode
 table(hse_data$Age)      # 2 modes, frequency table confirms
 
 # There are some 0 ages (babies under 12 months), so let's plot outliers
-# No outliers. And frequency table confirms. 
-# Plot box plot 
+# No outliers. And frequency table confirms.
+# Plot box plot
 hse_data %>%
   ggplot(aes(y = Age)) +
-  geom_boxplot(fill = "gray", outlier.color = "red", na.rm = TRUE) +
+  geom_boxplot(fill = "lightblue", outlier.color = "red", na.rm = TRUE) +
+  theme_bw() +
   labs(title = "Age Boxplot",
        y = "Age") +
   scale_x_continuous(breaks = NULL) # Remove x-axis labels
+
+# Plot histogram
+hse_data %>%
+  ggplot(aes(x = Age)) +
+  geom_histogram(fill = "lightblue", binwidth = 1) +
+  theme_bw() +
+  labs(title = "Age Histogram",
+       x = "Age",
+       y = "Frequency") +
+  scale_x_continuous(n.breaks = 10)
+
+##############################################################################
+# STEP 3: INFERENTIAL STATISTICS
+# a.	Run a significance test to find out which gender drinks more alcohol.
+# b.	Run a significance test to find out which region drinks the most alcohol.
+# c.	Investigate whether there is a statistical difference between
+#     men and women on the following variables:
+#     I.	Valid height.
+#     II.	Valid weight.
+# d.	What is the correlation between whether a person drinks nowadays, total
+#     household income, age at last birthday and gender?
+##############################################################################
+
+##############################################################################
+# a.	Run a significance test to find out which gender drinks more alcohol.
+# Variables
+# dnnow: Whether drink nowadays: Yes (1), No (2): categorical
+# Sex: Sex: Male (1), Female (2): categorical
+# Both categorical so don't need to check for normality
+# ==> Outcome: There is a significant
+##############################################################################
+
+# Contingency table
+sexdr_cont_tab <- table(hse_data$Sex, hse_data$dnnow)
+sexdr_cont_tab
+
+# Contingency table with labels
+sex_label <- factor(hse_data$Sex, levels = c(1,2), labels = c("Male", "Female"))
+dnnow_label <- factor(hse_data$dnnow, levels = c(1,2), labels = c("Yes", "No"))
+sexdr_labels_cont_tab <- table(sex_label, dnnow_label)
+sexdr_labels_cont_tab
+addmargins(sexdr_labels_cont_tab)
+
+# Contingentcy table with labels and percentages
+sexdr_pct_tab <- sexdr_labels_cont_tab %>%
+  prop.table( margin = 2) * 100
+sexdr_pct_tab
+addmargins(sexdr_pct_tab) # Add row and column totals
+
+# Chi-squared test of yes and no drink by gender
+sexdr_labels_cont_tab %>%
+  chisq.test() # Chi-squared test
+
+# Box plot of percentage men and women who drink
+sexdr_pct_df <- as.data.frame.table(sexdr_pct_tab)  # Convert to data frame
+colnames(sexdr_pct_df) <- c("Gender", "Drinks_Nowadays", "Percentage") # Rename
+sexdr_pct_df %>%
+  ggplot(aes(x = Gender, y = Percentage, fill = Drinks_Nowadays)) +
+  geom_bar(stat = "identity", position = "dodge", color = "black") +
+  theme_bw() +
+  scale_fill_brewer(palette = "Blues", direction = -1) +
+  labs(title = "Percentage Men and Women who Drink Nowadays",
+       x = "Gender", y = "Percentage", fill = "Drink Nowadays")
+
+##############################################################################
+# b.	Run a significance test to find out which region drinks the most alcohol.
+##############################################################################
+
+##############################################################################
+# c.	Investigate whether there is a statistical difference between
+#     men and women on the following variables:
+#     I.	Valid height.
+##############################################################################
+
+##############################################################################
+# c.	Investigate whether there is a statistical difference between
+#     men and women on the following variables:
+#     II.	Valid weight.
+##############################################################################
+
+##############################################################################
+# d.	What is the correlation between whether a person drinks nowadays, total
+#     household income, age at last birthday and gender?
+##############################################################################
